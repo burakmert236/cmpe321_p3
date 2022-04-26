776f9f4752c36b14dee76af7ede674506582426b
Burak Mert <burakmert236@gmail.com>
2022-04-26 16:06:10 +0300

    Instructor routes completed

--- queries/instructorQueries.js
+++ queries/instructorQueries.js
@@ -1,11 +1,36 @@
 var con = require('../db_config');
 
+const createCourse = (username, body, cb) => {
+    // first part not correct, must be checked
+    const query = `
+        INSERT INTO Course (course_id, name, credits, quota)
+        SELECT "${body.course_id}" AS course_id, "${body.name}" AS name, ${body.credits} AS credits, ${body.quota} AS quota
+        WHERE EXISTS (SELECT * FROM Classroom WHERE classroom_id = ${body.classroom_id} AND capacity >= ${body.quota} );
+
+        INSERT INTO Gives (username, course_id)
+        SELECT "${username}" AS username, "${body.course_id}" AS course_id
+        WHERE EXISTS (SELECT * FROM Classroom WHERE classroom_id = ${body.classroom_id} AND capacity >= ${body.quota} );
+
+        INSERT INTO Is_Given_In (classroom_id, course_id, time_slot)
+        SELECT ${body.classroom_id} AS classroom_id, "${body.course_id}" AS course_id, ${body.time_slot} AS time_slot
+        WHERE EXISTS (SELECT * FROM Classroom WHERE classroom_id = ${body.classroom_id} AND capacity >= ${body.quota} );
+    `
+    con.query(query, function (err, result, fields) {
+        console.log(result)
+        err || result[0].affectedRows === 0 ? cb(false) : cb(true);
+    });
+} 
+
 const availableClassrooms = (body, cb) => {
     // first part not correct, must be checked
     const query = `
         SELECT Cls.classroom_id, Cls.campus, Cls.capacity
-        FROM Classroom Cls, Is_Given_In Gvn
-        WHERE Cls.classroom_id = Gvn.classroom_id AND Gvn.time_slot != ${body.time_slot}
+        FROM Classroom Cls
+        WHERE Cls.classroom_id NOT IN (
+            SELECT Cls2.classroom_id 
+            FROM Classroom Cls2, Is_Given_In Gvn
+            WHERE Cls2.classroom_id = Gvn.classroom_id AND Gvn.time_slot = ${body.time_slot}
+        )
         UNION
         SELECT Cls.classroom_id, Cls.campus, Cls.capacity
         FROM Classroom Cls
@@ -21,6 +46,85 @@ const availableClassrooms = (body, cb) => {
     });
 } 
 
+const createPrerequisite = (body, cb) => {
+    // first part not correct, must be checked
+    const query = `
+        INSERT INTO Prerequisite_Of (course_id, prerequisite_id)
+        VALUES ("${body.course_id_m}", "${body.course_id_p}")
+    `
+    con.query(query, function (err, result, fields) {
+        console.log(err, result)
+        err || result.affectedRows === 0 ? cb(false) : cb(true);
+    });
+} 
+
+const getCourses = (username, cb) => {
+    // first part not correct, must be checked
+    const query = `
+        SELECT C.course_id, C.name, C.quota, Gvn.classroom_id, Gvn.time_slot
+        FROM Course C, Is_Given_In Gvn, Gives G
+        WHERE G.username = "${username}" AND G.course_id = C.course_id AND Gvn.course_id = C.course_id AND
+            G.course_id = Gvn.course_id
+
+    `
+    con.query(query, function (err, result, fields) {
+        console.log(err, result)
+        err || result.affectedRows === 0 ? cb(false) : cb(result);
+    });
+} 
+
+const getStudents = (username, body, cb) => {
+    // first part not correct, must be checked
+    const query = `
+        SELECT U.username, U.email, U.name, U.surname, S.student_id
+        FROM User U, Student S, Takes T
+        WHERE T.course_id = "${body.course_id}" AND S.username = U.username AND T.student_id = S.student_id AND
+            EXISTS (SELECT * FROM Gives WHERE username = "${username}" AND course_id = "${body.course_id}")
+
+    `
+    con.query(query, function (err, result, fields) {
+        console.log( err, result)
+        err || result.affectedRows === 0 ? cb(false) : cb(result);
+    });
+} 
+
+const updateName = (username, body, cb) => {
+    // first part not correct, must be checked
+    const query = `
+        UPDATE Course
+        SET name = "${body.name}"
+        WHERE course_id = "${body.course_id}"
+            AND EXISTS (SELECT * FROM Gives WHERE username = "${username}" AND course_id = "${body.course_id}")
+
+    `
+    con.query(query, function (err, result, fields) {
+        console.log(err, result)
+        err || result.affectedRows === 0 ? cb(false) : cb(true);
+    });
+} 
+
+
+const gradeStudent = (username, body, cb) => {
+    // first part not correct, must be checked
+    const query = `
+        UPDATE Takes
+        SET grade = ${body.grade}
+        WHERE course_id = "${body.course_id}" AND student_id = "${body.student_id}"
+            AND EXISTS (SELECT * FROM Gives WHERE username = "${username}" AND course_id = "${body.course_id}")
+
+    `
+    con.query(query, function (err, result, fields) {
+        console.log(err, result)
+        err || result.affectedRows === 0 ? cb(false) : cb(true);
+    });
+} 
+
 module.exports = {
     availableClassrooms,
+    createCourse,
+    createPrerequisite,
+    getCourses,
+    getStudents,
+    updateName,
+    gradeStudent
 }
--- routes/instructor.js
+++ routes/instructor.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var { 
-    availableClassrooms
+    availableClassrooms,
+    createCourse,
+    createPrerequisite,
+    getCourses,
+    getStudents,
+    updateName,
+    gradeStudent
   } = require('../queries/instructorQueries');
 
 /* GET admin home page. */
@@ -28,5 +34,124 @@ router.post('/available_classroom', function(req, res, next) {
       })
     });
 });
+
+
+/* GET add new course form. */
+router.get('/new_course', function(req, res, next) {
+  const user = req.user;
+  if(user) return res.render('instructor/newCourse', { user, title: "Instructor Home Page" })
+  return res.send("You are not an instructor")
+});
+/* POST add new course. */
+router.post('/new_course', function(req, res, next) {
+  createCourse(req.query.name, req.body, function(result) {
+    let message = ""
+    if(result) {
+      message = "Course is created successfuly"
+    } else {
+      message = "Course couldn't be created successfuly"
+    }
+    return res.render('instructor/newCourse', { user: {username: req.query.username}, title: "Instructor Home Page", message })
+  });
+});
+
+
+/* GET add new course form. */
+router.get('/new_prerequisite', function(req, res, next) {
+  const user = req.user;
+  if(user) return res.render('instructor/newPrerequisite', { user, title: "Instructor Home Page" })
+  return res.send("You are not an instructor")
+});
+/* POST add new course. */
+router.post('/new_prerequisite', function(req, res, next) {
+  createPrerequisite(req.body, function(result) {
+    let message = ""
+    if(result) {
+      message = "Prerequisite is created successfuly"
+    } else {
+      message = "Prerequisite couldn't be created successfuly"
+    }
+    return res.render('instructor/newPrerequisite', { user: {username: req.query.username}, title: "Instructor Home Page", message })
+  });
+});
+
+
+
+/* GET list of all courses page. */
+router.get('/courses', function(req, res, next) {
+  getCourses(req.query.name, function (result) {
+    return res.render('instructor/courses', { 
+      title: "Instructor Home Page", 
+      courses: result, 
+      message: result === false ? "Courses couldn't be listed" : "" 
+    })
+  });
+});
+
+
+/* GET list of all students page. */
+router.get('/students', function(req, res, next) {
+  const user = req.user;
+  if(user) return res.render('instructor/students', { user, title: "Instructor Home Page" })
+  return res.send("You are not an instructor")
+});
+/* POST list of students. */
+router.post('/students', function(req, res, next) {
+  getStudents(req.query.name, req.body, function(result) {
+    let message = ""
+    if(!result) {
+      message = "Students couldn't be listed"
+    }
+    return res.render('instructor/students', { 
+      user: {username: req.query.username}, 
+      title: "Instructor Home Page", 
+      message,
+      students: result,
+      course_id: req.body.course_id,
+    })
+  });
+});
+
+
+/* GET update name form. */
+router.get('/update_name', function(req, res, next) {
+  const user = req.user;
+  if(user) return res.render('instructor/updateName', { user, title: "Instructor Home Page" })
+  return res.send("You are not an instructor")
+});
+/* POST update name. */
+router.post('/update_name', function(req, res, next) {
+  updateName(req.query.name, req.body, function(result) {
+    let message = ""
+    if(result) {
+      message = "Name is updated successfuly"
+    } else {
+      message = "Name couldn't be updated successfuly"
+    }
+    return res.render('instructor/updateName', { user: {username: req.query.username}, title: "Instructor Home Page", message })
+  });
+});
+
+
+
+/* GET grade student form. */
+router.get('/grade_student', function(req, res, next) {
+  const user = req.user;
+  if(user) return res.render('instructor/gradeStudent', { user, title: "Instructor Home Page" })
+  return res.send("You are not an instructor")
+});
+/* POST grade student. */
+router.post('/grade_student', function(req, res, next) {
+  gradeStudent(req.query.name, req.body, function(result) {
+    let message = ""
+    if(result) {
+      message = "Grade is created successfuly"
+    } else {
+      message = "Grade couldn't be created successfuly"
+    }
+    return res.render('instructor/gradeStudent', { user: {username: req.query.username}, title: "Instructor Home Page", message })
+  });
+});
+
 
 module.exports = router;
